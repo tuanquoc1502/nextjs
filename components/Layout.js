f9c9c059ceb62fb9ab44cb0a5301bb2de72b6a5b
your name <tuanquoc150222@gmail.com>
2022-06-05 21:29:03 +0700

    base2 project

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,18 @@
 import Head from "next/head";
 
-const Layout = () => {
-    return (
-        <Head>1</Head>
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Next</title>
+      </Head>
+      <header></header>
+      <main>{children}</main>
+    </>
+  );
+};
+
+export default Layout;
