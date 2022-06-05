109528f8097f46aefa2605ebc47cc4bdb78cc8d0
your name <tuanquoc150222@gmail.com>
2022-06-05 21:42:21 +0700

    base3 project

--- components/Layout.js
+++ components/Layout.js
@@ -1,17 +1,20 @@
 import Head from "next/head";
+import NavbarMenu from "./NavbarMenu";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="wrapper">
       <Head>
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Next</title>
       </Head>
-      <header></header>
+      <header>
+        <NavbarMenu />
+      </header>
       <main>{children}</main>
-    </>
+    </div>
   );
 };
 
---
+++ components/NavbarMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavbarMenu = () => {
+  return (
+    <div className="menus">
+      <div className="item">Post</div>
+      <div className="item">About</div>
+    </div>
+  );
+};
+
+export default NavbarMenu;
